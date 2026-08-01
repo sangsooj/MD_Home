bd2560e6a2d961f798512d413906e208f304332e
sangsooj <sangsoo.jeong@gmail.com>
2026-08-01 13:37:29 +0900

    feat: replace site logo

--- scripts/prepare-static.mjs
+++ scripts/prepare-static.mjs
@@ -3,5 +3,6 @@ import { cp, mkdir, rm } from "node:fs/promises";
 await rm("public", { recursive: true, force: true });
 await mkdir("public", { recursive: true });
 await cp("index.html", "public/index.html");
+await cp("assets", "public/assets", { recursive: true });
 await cp("notices", "public/notices", { recursive: true });
 await cp("CNAME", "public/CNAME");
